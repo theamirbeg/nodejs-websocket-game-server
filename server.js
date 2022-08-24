f827bc530ca61d13cb65f82753232cd44927261e
Mohammad Amir Beg <47865119+theamirbeg@users.noreply.github.com>
2022-08-25 01:09:20 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const ws = require('nodejs-websocket');
+const port = process.env.PORT || 3000
+
+const server = ws.createServer((connection)=>{
+    console.log("New connection");
+
+    connection.on("text", (str)=>{
+        console.log("Recieved: " + str); 
+        connection.send(str.toUpperCase());
+    });
+
+    connection.on("close", (code, reason)=>{
+        console.log("Connection closed");
+    });
+
+}).listen(port);
+
+
+
+server.on("connection", (client)=>{
+    client.send("Hello client");
+});
+
+server.on("listening", ()=>{
+    console.log("Listening on port " + port);
+});
